82778efc6732804d6ae3657dff984ae69b2b8933
martindustry <marcin.exn@gmail.com>
2022-11-15 21:01:19 +0100

    feat: implement login to Browsable API #4

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
+    path("api-auth/", include("rest_framework.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
