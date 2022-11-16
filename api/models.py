766ed005ab90a7268e154c1b0a0bab0001151037
martindustry <marcin.exn@gmail.com>
2022-11-16 13:21:48 +0100

    feat: add account tier models

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.core.validators import FileExtensionValidator
 from django.db import models
 
@@ -10,3 +11,18 @@ class Image(models.Model):
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE
     )
+
+
+class AccountTier(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    thumbnail_sizes = models.ManyToManyField("Size", blank=True)
+    orginal_image = models.BooleanField()
+    generate_links = models.BooleanField()
+
+
+class Size(models.Model):
+    height = models.IntegerField(unique=True)
+
+
+class User(AbstractUser):
+    tier = models.ForeignKey(AccountTier, on_delete=models.CASCADE, null=True)
