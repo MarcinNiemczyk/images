23c1034211e173915539594397695e7ef4fb8e91
martindustry <marcin.exn@gmail.com>
2022-11-16 11:56:16 +0100

    feat: validate uploaded image #3

--- api/models.py
+++ api/models.py
@@ -1,9 +1,12 @@
 from django.conf import settings
+from django.core.validators import FileExtensionValidator
 from django.db import models
 
 
 class Image(models.Model):
-    image = models.ImageField()
+    image = models.ImageField(
+        validators=[FileExtensionValidator(allowed_extensions=["jpg", "png"])]
+    )
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE
     )
