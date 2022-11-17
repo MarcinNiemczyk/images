9c2e820b079de1d4b2f38ae0ca60c00a5d98f15e
martindustry <marcin.exn@gmail.com>
2022-11-17 21:43:24 +0100

    fix: create thumbnails for all sizes on upload
    * Generate all thumbnail sizes in case user decides to upgrade his account tier in the future
    
    * Compromise between database load and scenario of longer time response to resize all user images to thumbnails

--- api/signals.py
+++ api/signals.py
@@ -3,14 +3,14 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from PIL import Image as ImageFile
 
-from api.models import Image, Thumbnail
+from api.models import Image, Size, Thumbnail
 
 
 @receiver(post_save, sender=Image)
 def create_thumbnails(sender, instance, created, **kwargs):
     if not created:
         return
-    sizes = instance.author.tier.thumbnail_sizes.all()
+    sizes = Size.objects.all()
     for size in sizes:
         img = ImageFile.open(instance.image.path)
         height = size.height
