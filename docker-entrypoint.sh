2e6ffde4098790b1e045093ef19a2acb1762773d
martindustry <marcin.exn@gmail.com>
2022-11-22 13:43:21 +0100

    build: improve docker build
    * Add custom migrations to reduce amount of commands to initialize project
    
    * Apply migrate command to docker-compose

--- docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-python manage.py makemigrations api
-python manage.py migrate
-python manage.py loaddata fixtures/builtin_tiers.json
-DJANGO_SUPERUSER_PASSWORD=admin python manage.py createsuperuser --no-input --username=admin  --email=admin@example.com
